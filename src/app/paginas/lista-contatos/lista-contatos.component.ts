46c8f5de2ce8441750b55d918d263bf74b035c19
LuizCowBTF <luizcowbtf@gmail.com>
2024-03-18 15:09:17 -0300

    update

--- src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -1,20 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { ContatoComponent } from '../../componentes/contato/contato.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
 import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
-
-interface Contato {
-  id: number
-  nome: string
-  telefone: string
-}
-
-import agenda from '../../agenda.json';
-import { RouterLink } from '@angular/router';
+import { ContatoService } from '../../services/contato.service';
+import { Contato } from '../../componentes/contato/contato';
 
 @Component({
   selector: 'app-lista-contatos',
@@ -27,27 +22,38 @@ import { RouterLink } from '@angular/router';
     CabecalhoComponent,
     SeparadorComponent,
     ContatoComponent,
-    FormularioContatoComponent],
+    FormularioContatoComponent,
+  ],
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css'
 })
-export class ListaContatosComponent {
+export class ListaContatosComponent implements OnInit{
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
-  contatos: Contato[] = agenda;
-  filtroPorTexto: string = '';
+  contatos: Contato[] = [];
+  filtroPorTexto: string = ''
 
-  filtrarContatosPorLetraInicial(letra: string): Contato[] {
-    return this.filtrarContatoPorTexto().filter(contato => {
-      return contato.nome.toLowerCase().startsWith(letra);
+  constructor(private contatoService: ContatoService) {}
+
+  ngOnInit() {
+    this.contatos = this.contatoService.obterContatos();
+  }
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
     })
   }
 
-  filtrarContatoPorTexto(): Contato[] {
-    if(!this.filtroPorTexto) {
-      return this.contatos
-    }
-    return this.contatos.filter(contato => {
-      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase());
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase());
     })
   }
 }
--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Contato } from '../componentes/contato/contato';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContatoService {
 
-  constructor() { }
+  private contatos: Contato[] = [
+    {"id": 1, "nome": "Ana", "telefone": "29 278869420", "email": "email@email.com"},
+  ]
+
+  constructor() {
+    const contatosLocalStorageString = localStorage.getItem('contatos');
+    const contatosLocalStorage = contatosLocalStorageString ? JSON.parse(contatosLocalStorageString) : null;
+
+    this.contatos = contatosLocalStorage || null;
+    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  }
+
+  obterContatos() {
+    return this.contatos;
+  }
+
+  salvarContato(contato: Contato) {
+    this.contatos.push(contato);
+    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  }
 }
